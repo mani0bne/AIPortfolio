6695032f585665d2d0880277ca32a384a1a5e27c
Manihas Netha N <119604785+mani0bne@users.noreply.github.com>
2026-09-01 17:47:03 +0530

    Delete backend directory

--- backend/routes/saveRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { savePortfolio } from "../controller/savePortfolioController.js";
-
-const router = express.Router();
-
-router.post("/save-portfolio", savePortfolio);
-
-export default router;
